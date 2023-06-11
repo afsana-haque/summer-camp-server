49963e4cc9178f4ee951be8f99478e92a5286e8b
afsana-haque <afsana.haque3301@gmail.com>
2023-06-12 02:51:26 +0600

    addded instructor data

--- index.js
+++ index.js
@@ -31,12 +31,19 @@ async function run() {
     //await client.connect();
 
     const classesCollection = client.db("musicSchhol").collection("classes");
+    const instructorsCollection = client.db("musicSchhol").collection("instructors");
 
+    //Classes data
     app.get('/classes', async(req, res) => {
       const result = await classesCollection.find().sort({students: -1}).toArray();
       res.send(result);
     })
 
+    //Instructors data
+    app.get('/instructors', async(req, res) => {
+      const result = await instructorsCollection.find().sort({students: -1}).toArray();
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
