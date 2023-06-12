16b38dd732c2a2b3c6aaad0ea4f93f31c69b69e5
afsana-haque <afsana.haque3301@gmail.com>
2023-06-12 15:48:15 +0600

    added classes cart data

--- index.js
+++ index.js
@@ -47,6 +47,16 @@ async function run() {
     })
 
     //cart Collection 
+    app.get('/carts', async(req, res) => {
+      const email = req.query.email;
+      if(!email){
+        res.send([]);
+      }
+      const query = {email: email};
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    })
+
     app.post('/carts', async (req, res) => {
       const classes = req.body;
       console.log(classes);
