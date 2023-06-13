daf7a39412a3dcdd8e18fe553249405c5c30c7e7
afsana-haque <afsana.haque3301@gmail.com>
2023-06-14 00:09:11 +0600

    added user existing

--- index.js
+++ index.js
@@ -38,6 +38,11 @@ async function run() {
     //User apis
     app.post('/users', async(req, res) => {
       const user = req.body;
+      const query = {email: user.email}
+      const existingUser = await usersCollection.findOne(query);
+      if(existingUser) {
+        return res.send({message: 'User already exists'})
+      }
       const result = await usersCollection.insertOne(user);
       res.send(result);
     })
